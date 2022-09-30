f9750e635135144c2b1b1d881786b3f693b04144
erebus-01 <phamhiep.it.epu@gmail.com>
2022-10-01 00:33:12 +0700

    interface settings

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Profile(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  id_user = models.IntegerField()
+  story = models.TextField(blank=True)
+  firstName = models.CharField(max_length=50, blank=True)
+  lastName = models.CharField(max_length=50, blank=True)
+  website = models.CharField(max_length=20, blank=True)
+  profileimg = models.ImageField(upload_to='profile_images', default='blank_profile.png')
+
+  def __str__(self):
+    return self.user.username
